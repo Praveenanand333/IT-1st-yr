2bc2ac21129d8e6d7a89882d413b5f7a6c7f68eb
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2022-05-21 14:04:44 +0530

    Add files via upload

---
+++ sample2.php
@@ -0,0 +1,15 @@
+<!-- 1.	Write two different PHP scripts to demonstrate passing variables to a URL. -->
+<html>
+    <head>
+        <title>
+sample1
+        </title>
+    </head>
+    <body>
+<form action="sample3.php" method="post">
+    Username:<input type="text" name="username"><br>
+    Phone number<input type="tel" name="number"><br>
+    <input type="submit">
+</form>
+    </body>
+    </html>
---
+++ sample3.php
@@ -0,0 +1,7 @@
+<?php
+$username=$_REQUEST['username'];
+$number=$_REQUEST['number'];
+echo 'the entered name is',$username;
+echo "<br>";
+echo 'entered number is ',$number;
+?>
