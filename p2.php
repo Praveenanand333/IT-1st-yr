26e4c84be2ffb75bc40cd704fc2831772f0d76f3
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2022-06-17 22:15:52 +0530

    Update p2.php

--- p2.php
+++ p2.php
@@ -29,6 +29,13 @@
             if($_POST['sl1'] == 'Ascending'){
                 sort($arr);
                 sort($arr2);
+                sort($arr3);
+            }
+            if($_POST['sl1'] == 'Descending'){
+                        rsort($arr);
+                        rsort($arr2);
+                    rsort($arr3);
+                }
                 echo "<form method='post'>
                 <select>";
             foreach($arr as $i){
@@ -48,57 +55,25 @@
                 echo "<option value='{$i}'>{$i}</option>";
             }
             echo "</select><br><br>";
-            echo "<input type='submit' value='Submit'></form>";
+            echo "<input type='submit' value='Submit'></form>";   
         }
-        else{
-                rsort($arr);
-                rsort($arr2);
-                echo "<form method='post'>
-                <select>";
-            foreach($arr as $i){
-                echo "<option>{$i}</option>";
-            }
-            echo "</select>";
-            echo "<br><br>";
-            echo "<select>";
-            foreach($arr2 as $i){
-                echo "<option>{$i}</option>";
-            }
-            echo "</select>";
-            echo "<br><br>";
-
-            echo "<select name='sl1'>";
-            foreach($arr3 as $i){
-                echo "<option value='{$i}'>{$i}</option>";
-            }
-            echo "</select><br><br>";
-            echo "<input type='submit' value='Submit'></form>";
-        }
-            }
-        
         else{
             echo "<form method='post' action="."'p2.php'>
                 <select>";
             foreach($arr as $i){
-                echo "<option>{$i}</option>";
-            }
+                echo "<option>{$i}</option>"; }
             echo "</select>";
             echo "<br><br>";
             echo "<select>";
             foreach($arr2 as $i){
-                echo "<option>{$i}</option>";
-            }
+                echo "<option>{$i}</option>";          }
             echo "</select>";
             echo "<br><br>";
-
             echo "<select name='sl1'>";
             foreach($arr3 as $i){
-                echo "<option value='{$i}'>{$i}</option>";
-            }
+                echo "<option value='{$i}'>{$i}</option>"; }
             echo "</select><br><br>";
-            echo "<input type='submit' value='Submit'></form>";
-
-        }
+            echo "<input type='submit' value='Submit'></form>";}
     ?>
 </body>
 </html>
