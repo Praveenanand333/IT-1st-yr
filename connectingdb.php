a09e7e26235e108da3786d7db1462e0b971f4789
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2022-06-29 15:30:37 +0530

    Update connectingdb.php

--- connectingdb.php
+++ connectingdb.php
@@ -17,10 +17,11 @@ $database='bookdb';
  if($conn->connect_error){
  die('connection error'.$conn->connect_error);
  }
-$query0="insert into book(Id,Name,Purchaser,Price) values(2,'python','ram',3000)";
+$query0="insert into book(Id,Name,Purchaser,Price) values(5,'python','ram',3000)";
 $result0=$conn->query($query0);
  $query='select * from book';
  $result=$conn->query($query);
+ echo 'After inserting<br>';
 echo '<table>';
 while($row =$result->fetch_assoc()){
 	echo '<tr>';
@@ -28,5 +29,16 @@ while($row =$result->fetch_assoc()){
  	echo '</tr>';
  }
  echo '</table>';
+ $query2='delete from book where Id=5';
+ $result2=$conn->query($query2);
+ echo 'After deleting<br>';
  
+ echo '<table>';
+ $result3=$conn->query($query);
+while($row =$result3->fetch_assoc()){
+	echo '<tr>';
+ echo '<td>'.$row['Id'].'</td>'.'<td>'.$row['Name'].'</td>'.'<td>'.$row['Purchaser'].'</td>'.'<td>'.$row['Price'].'</td>';
+ 	echo '</tr>';
+ }
+ echo '</table>';
  ?>
